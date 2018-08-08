d949f946366b37814481d464bd72e9987749b12b
Nasim Ahmed <ahmednasim28@gmail.com>
2018-08-07 20:55:30 -0400

    Updated server file and added two routes: students and index

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var expressValidator = require('express-validator');
 var session = require('express-session');
 var flash = require('connect-flash');
 var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/StudentInformationSystem');
+mongoose.connect('mongodb://localhost:27017/StudentInformationSystem', {useNewUrlParser: true});
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
@@ -18,3 +18,53 @@ var students = require('./routes/students');
 var app = express();
 
 // View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret', // the word secret, you can change it to whatever you like
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+
+// Connect Flash
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use('/', routes);
+app.use('/students', students);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
